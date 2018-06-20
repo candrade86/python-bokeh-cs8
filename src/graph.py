813c4d3d1dc710ce0251c597777f32cdaeaee55f
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-20 18:41:35 -0400

    completed

---
+++ src/graph.py
@@ -0,0 +1,33 @@
+class Edge:
+    def __init__(self, destination):
+        self.destination = destination
+
+class Vertex:
+    def __init__(self, value, **pos): # asterix means list of keyword args
+        self.value = value
+        self.color = 'white'
+        self.pos = pos
+        self.edges = []
+
+class Graph: 
+    def __init__(self):
+        self.vertexes = []
+
+    def debug_create_test_data(self):
+        debug_vertex_1 = Vertex('t1', x=40, y=40) 
+        debug_vertex_2 = Vertex('t2', x=140, y=140) 
+        debug_vertex_3 = Vertex('t3', x=240, y=240)
+        debug_vertex_4 = Vertex('t4', x=310, y=110)
+        debug_vertex_5 = Vertex('t5', x=400, y=400)  
+
+        debug_edge_1 = Edge(debug_vertex_2)
+        debug_edge_2 = Edge(debug_vertex_3)
+        debug_edge_3 = Edge(debug_vertex_4)
+        debug_edge_4 = Edge(debug_vertex_5)
+
+        debug_vertex_1.edges.append(debug_edge_1)
+        debug_vertex_2.edges.append(debug_edge_2)
+        debug_vertex_3.edges.append(debug_edge_3)
+        debug_vertex_4.edges.append(debug_edge_4)
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+        self.vertexes.extend([debug_vertex_3, debug_vertex_4, debug_vertex_5])
