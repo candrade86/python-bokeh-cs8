d7bee9db2dd8f97b935e15d04644c7a335176572
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-21 18:41:45 -0400

    Working on it!

--- src/draw.py
+++ src/draw.py
@@ -2,24 +2,27 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Triangle 
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Triangle, ColumnDataSource, Range1d, LabelSet, Label 
 from bokeh.palettes import Spectral8
 
 from graph import *
 
+WIDTH = 500
+HEIGHT = 500 
+
 graph_data = Graph()
 graph_data.debug_create_test_data()
-print(graph_data.vertexes)
+graph_data.bfs(graph_data.vertexes[0])
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-color_list = Spectral8
+color_list = []
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+plot = figure(title='Carlos\' Super Cool Graph', x_range=(0, WIDTH), y_range=(0, HEIGHT),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
@@ -28,9 +31,19 @@ graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Triangle(size=60, angle=60, fill_color='color')
 
+# this is drawing the edges from start to end
+start_indexes = []
+end_indexes = []
+
+for start_index, vertex in enumerate(graph_data.vertexes):
+    for e in vertex.edges:
+        start_indexes.append(start_index)
+        end_indexes.append(graph_data.vertexes.index(e.destination))
+
+
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=start_indexes, # a list of indexes to start edges from
+    end=end_indexes) # a list of vertex indexes to end edges at
 
 ### start of layout code
 
@@ -40,7 +53,36 @@ y = [v.pos['y'] for v in graph_data.vertexes]
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
+
+
+
 plot.renderers.append(graph)
+
+
+source = ColumnDataSource(data=dict(height=[66, 71, 72, 68, 58, 62],
+                                    weight=[165, 189, 220, 141, 260, 174],
+                                    names=['Mark', 'Amir', 'Matt', 'Greg',
+                                           'Owen', 'Juan']))
+
+# Create a new dictionary to use as a data source, with three lists in it, ordered in the same way as verteses
+# List of x valuses
+#list of y valuses
+#list of labels
+ 
+value = [v.value for v in graph_data.vertexes] #TODO: Possible optimization: We run through this loop three times
+
+label_source = ColumnDataSource(data=dict(x=x, y=y,  v=value))
+
+p = figure(title='Dist. of 10th Grade Students at Lee High',
+           x_range=Range1d(140, 275))
+p.scatter(x='weight', y='height', size=8, source=source)
+p.xaxis[0].axis_label = 'Weight (lbs)'
+p.yaxis[0].axis_label = 'Height (in)'
+
+labels = LabelSet(x='x', y='y', text='v', level='overlay',
+              source=label_source, render_mode='canvas', text_align='center', text_baseline='middle')
+
+plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
--- src/graph.py
+++ src/graph.py
@@ -1,3 +1,5 @@
+import random
+
 class Edge:
     def __init__(self, destination):
         self.destination = destination
@@ -16,18 +18,47 @@ class Graph:
     def debug_create_test_data(self):
         debug_vertex_1 = Vertex('t1', x=40, y=40) 
         debug_vertex_2 = Vertex('t2', x=140, y=140) 
-        debug_vertex_3 = Vertex('t3', x=240, y=240)
+        debug_vertex_3 = Vertex('t3', x=100, y=240)
         debug_vertex_4 = Vertex('t4', x=310, y=110)
-        debug_vertex_5 = Vertex('t5', x=400, y=400)  
+        debug_vertex_5 = Vertex('t5', x=400, y=400)
+        debug_vertex_6 = Vertex('t6', x=100, y=300)  
+        debug_vertex_7 = Vertex('t7', x=400, y=200) 
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_edge_2 = Edge(debug_vertex_3)
         debug_edge_3 = Edge(debug_vertex_4)
         debug_edge_4 = Edge(debug_vertex_5)
+        debug_edge_5 = Edge(debug_vertex_6)
+        debug_edge_6 = Edge(debug_vertex_7)
 
         debug_vertex_1.edges.append(debug_edge_1)
         debug_vertex_2.edges.append(debug_edge_2)
         debug_vertex_3.edges.append(debug_edge_3)
         debug_vertex_4.edges.append(debug_edge_4)
+        debug_vertex_5.edges.append(debug_edge_5)
+        debug_vertex_6.edges.append(debug_edge_6)
         self.vertexes.extend([debug_vertex_1, debug_vertex_2])
-        self.vertexes.extend([debug_vertex_3, debug_vertex_4, debug_vertex_5])
+        self.vertexes.extend([debug_vertex_3, debug_vertex_4, debug_vertex_5, debug_vertex_6, debug_vertex_7])
+        # connect bi-directionally for bfs to work
+    def bfs(self, start):
+        # random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)] 
+        random_color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+
+        queue = []
+        found = []
+
+        queue.append(start)
+        found.append(start)
+
+        start.color = random_color
+
+        while len(queue) > 0:
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    queue.append(edge.destination)
+                    edge.destination.color = random_color
+            queue.pop(0) #TODO: Look into collections.dequeue
+
+        return found        
